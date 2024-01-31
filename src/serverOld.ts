f04e3fb7a238d2303476bf143099cba493a66ccc
banfiandras <andras.banfi.hun@gmail.com>
2024-01-31 14:24:22 +0100

    first commit

---
+++ src/serverOld.ts
@@ -0,0 +1,19 @@
+
+import express, {Express, NextFunction, Request, Response } from 'express';
+
+function loggerMiddleware(request: Request, response:Response, next: NextFunction) {
+    console.log(`${request.method} ${request.path}`);
+    next();
+  }
+   
+
+const app: Express = express();
+ 
+app.use(express.json());
+app.use(loggerMiddleware);
+
+app.get('/', (request: Request, response: Response) => {
+  response.send(request.body);
+});
+ 
+app.listen(5000);
